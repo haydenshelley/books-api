fc15a2e7180aab8fea26e2cd058c418efb07ee98
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-08 13:39:54 -0700

    initial commit, create Book model

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# books = CSV.read("books.csv")
+
+# i = 1
+# while i < books.length
+#   book = Book.new(
+#   title: books[i][0],
+#   price: books[i][1],
+#   availability: books[i][2]
+#   )
+#   book.save
+#   i += 1
+# end
