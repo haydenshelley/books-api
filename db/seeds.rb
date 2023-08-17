7c5968a83c576188758d867c156a935091dd598c
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-17 13:58:38 -0700

    add books index html, add books show html

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-# books = CSV.read("books.csv")
+books = CSV.read("books.csv")
 
-# i = 1
-# while i < books.length
-#   book = Book.new(
-#   title: books[i][0],
-#   price: books[i][1],
-#   availability: books[i][2]
-#   )
-#   book.save
-#   i += 1
-# end
+i = 1
+while i < books.length
+  book = Book.new(
+  title: books[i][0],
+  price: books[i][1],
+  availability: books[i][2]
+  )
+  book.save
+  i += 1
+end
 
 # 20.times do
 #   store = Store.create(
